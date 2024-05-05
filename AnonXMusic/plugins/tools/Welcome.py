0b598d57524ac7aab097ce33340772f9562ab9da
ShubhoEditx <166376289+ShubhoEditx@users.noreply.github.com>
2024-05-05 16:29:19 +0530

    Update Welcome.py

--- AnonXMusic/plugins/tools/Welcome.py
+++ AnonXMusic/plugins/tools/Welcome.py
@@ -131,7 +131,7 @@ async def greet_group(_, member: ChatMemberUpdated):
 𑁍 𝐏ᴏᴡᴇʀᴇᴅ ʙʏ ➥ 𓆩 𝐒 𝐇 𝐔 𝐁 𝐇 𝐎 𓆪
 
     ╰───── • ◆ • ─────╯"""
-, reply markup=InlineKeyboardMarkup(
+ ,reply markup=InlineKeyboardMarkup(
 [
 [InlineKeyboardButton(f"ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/SohiniMusicBot?startgroup=new"),
 ]
